fba6fb204c3f2c089faab6633b75f27d483946f6
techsayright <darshan2001.dp@gmail.com>
2022-12-06 17:45:51 +0530

    init

--- ksql.sh
+++ ksql.sh
@@ -5,17 +5,17 @@ docker-compose exec -T ksqldb-cli ksql http://ksqldb-server:8088 <<-EOF
     set 'cache.max.bytes.buffering'='10000000';
     set 'auto.offset.reset'='earliest';
 
+    CREATE STREAM resto_src WITH (KAFKA_TOPIC='demo.business.resto', VALUE_FORMAT='AVRO', WRAP_SINGLE_VALUE=false);
 
-    CREATE STREAM courses_src WITH (KAFKA_TOPIC='demo.class.courses', VALUE_FORMAT='AVRO');
-
-    CREATE STREAM courses1 WITH (KAFKA_TOPIC='demo.class.courses', VALUE_FORMAT='AVRO');
+    select EXTRACTJSONFIELD(ROWVAL, '$.operationType') AS operationType, EXTRACTJSONFIELD(ROWVAL, '$.documentKey._id') AS _id, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.address.building') AS building ,EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.address.street') AS street ,
+    EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.address.zipcode') AS zipcode ,EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.borough') AS borough, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.cuisine') AS cuisine, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.grades') AS grades, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.name') AS name, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.restaurant_id') AS restaurant_id, EXTRACTJSONFIELD(ROWVAL, '$.ns.db') AS database_name, EXTRACTJSONFIELD(ROWVAL, '$.ns.coll') AS collection_name, rowtime AS time from resto_src emit changes;
 
     CREATE OR REPLACE STREAM class_boost WITH (
     kafka_topic = 'demo.class.class_boost',
     VALUE_FORMAT='AVRO'
 )   AS
-    SELECT *, rowtime AS time FROM courses1
-    EMIT CHANGES;
+    select EXTRACTJSONFIELD(ROWVAL, '$.operationType') AS operationType, EXTRACTJSONFIELD(ROWVAL, '$.documentKey._id') AS _id, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.address.building') AS building ,EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.address.street') AS street ,
+    EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.address.zipcode') AS zipcode ,EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.borough') AS borough, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.cuisine') AS cuisine, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.grades') AS grades, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.name') AS name, EXTRACTJSONFIELD(ROWVAL, '$.fullDocument.restaurant_id') AS restaurant_id, EXTRACTJSONFIELD(ROWVAL, '$.ns.db') AS database_name, EXTRACTJSONFIELD(ROWVAL, '$.ns.coll') AS collection_name, rowtime AS time from resto_src emit changes;
 
 
 EOF
--- mongo/init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mongoimport --uri mongodb://mongo1:27017/class --collection courses --drop --type json --mode=upsert --file "home/config/Data/courses.json" --jsonArray
